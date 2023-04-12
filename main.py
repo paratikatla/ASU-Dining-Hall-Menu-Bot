21820d10583be57dea8ca0ad8e7ec35ecbb153fb
Pranav Aratikatla <pranav.aratikatla@gmail.com>
2023-04-12 07:14:35 -0700

    completed manzanita and tooker dining halls

--- main.py
+++ main.py
@@ -441,7 +441,6 @@ async def tooker_menu(ctx, arg1):
     
     await browser.close()
     
-
 @bot.command(name='manzanita')
 async def manzy_menu(ctx, arg1):
     
@@ -732,11 +731,303 @@ async def manzy_menu(ctx, arg1):
 
     await ctx.send(embed=embed)
     
+@bot.command(name='manzy')
+async def manzy_menu(ctx, arg1):
+    
+    browser = await launch()
+    page = await browser.newPage()
+    
+    page.setDefaultNavigationTimeout(60000)
+    await page.goto('https://asu.campusdish.com/en/diningvenues/tempemanzanitaresidentialrestaurant/')
+    
+    try:
+        subscription_modal_button = await page.xpath('//*[@id="modal-root-mail-subsription"]/div/div/div/div/div/div[1]/button')
+        if subscription_modal_button:
+            await asyncio.wait_for(subscription_modal_button[0].click(), timeout=10)
+    except TimeoutError:
+        pass
+
+    try:
+        cookie_banner_button = await page.xpath('//*[@id="onetrust-close-btn-container"]/button')
+        if cookie_banner_button:
+            await asyncio.wait_for(cookie_banner_button[0].click(), timeout=10)
+    except TimeoutError:
+        pass
+    
+    food_dict = {}
+    
+    async def scrape_food(xpath, key):
+        try:
+            food_element = await page.xpath(xpath)
+            food_element = food_element[0]
+            food_elements = await food_element.querySelectorAll('div.sc-tQuYZ.gvgoZc > button > h3 > span')
+            
+            foods = []
+            
+            for food in food_elements:
+                food_text = await page.evaluate('(element) => element.textContent', food)
+                if food_text:
+                    foods.append(food_text)
+                
+            food_dict.update({key: foods})
+        except:
+            pass
+        
+    requestedMeal = arg1.lower()
+    
+    if(requestedMeal == 'breakfast'):
+            if(day == 5 or day == 6):
+                await ctx.send("Today is a weekend which means that Manzanita dining hall does not serve Breakfast, perhaps try asking for lunch instead")
+                
+            else:
+                await asyncio.sleep(2)
+
+                meal = await page.querySelector('.ChoosenMeal')
+                meal = await page.evaluate('(element) => element.textContent', meal)
+
+                if meal != 'Breakfast':
+                    try:
+                        swapButton = await page.waitForSelector('.DateMealFilterButton', timeout=5000)
+                        await swapButton.click()
+                    except:
+                        pass
+
+                    dropDown = await page.waitForSelector('.css-1t70p0u-control', timeout=5000)
+                    await dropDown.click()
+
+                    try:
+                        breakfastOption = await page.waitForXPath('//div[text()="Breakfast"]', timeout=5000)
+                        await breakfastOption.click()
+                    except:
+                        await ctx.send("Sorry but it does not appear that there is a Breakfast option today at Manzanita dining...")
+
+                    doneButton = await page.waitForSelector('#modal-root > div > div > div > div > div.sc-cCsOjp.gvlGSX > button.sc-bczRLJ.sc-gsnTZi.gObyWR.SlTeX.Done', timeout=5000)
+
+                    await doneButton.click()
+                    await page.waitForSelector('.ChoosenMeal', timeout=60000)
+
+                    await asyncio.sleep(2)
+                    
+                await scrape_food('//*[@id="13988"]', 'Daily Root')
+                await scrape_food('//*[@id="9139"]', 'Home Zone')
+                await scrape_food('//*[@id="9141"]', 'Salad Bar')
+                await scrape_food('//*[@id="9138"]', 'Grill')
+                
+                dailyRoot = food_dict['Daily Root']
+                dailyRoot = ', '.join(dailyRoot)
+                
+                homeZone = food_dict['Home Zone']
+                homeZone = ', '.join(homeZone)
+                
+                saladBar = food_dict['Salad Bar']
+                saladBar = ', '.join(saladBar)
+                
+                grill = food_dict['Grill']
+                grill = ', '.join(grill)
+                
+                embed = discord.Embed(title='Manzanita Dining Hall Breakfast', description= 'Manzanita Dining Hall Breakfast Menu')
+        
+                embed.set_thumbnail(url="https://i.imgur.com/DKR50qf.jpg")
+                
+                try:
+                    embed.add_field(name="Daily Root:", value=dailyRoot, inline=False)
+                except:
+                    pass
+                
+                try:
+                    embed.add_field(name="Home Zone:", value=homeZone, inline=False)
+                except:
+                    pass
+                
+                try:
+                    embed.add_field(name='Salad Bar:', value=saladBar, inline=False)
+                except:
+                    pass
+                
+                try:
+                    embed.add_field(name='Grill:', value=grill, inline=True)
+                except:
+                    pass
+
+                embed.set_image(url="https://i.imgur.com/GSx3eR2.png")
+                
+    if(requestedMeal == 'lunch'):
+        
+        if(day == 5 or day == 6):
+            await ctx.send("Sorry, but today appears to be a weekend which means that the Manzanita Dining Hall is not serving lunch, perhaps try requesting the Brunch menu instead")
+
+        else:
+            await asyncio.sleep(2)
+
+            meal = await page.querySelector('.ChoosenMeal')
+            meal = await page.evaluate('(element) => element.textContent', meal)
+
+            if meal != 'Lunch':
+                try:
+                    swapButton = await page.waitForSelector('.DateMealFilterButton', timeout=5000)
+                    await swapButton.click()
+                except:
+                    pass
+
+                dropDown = await page.waitForSelector('.css-1t70p0u-control', timeout=5000)
+                await dropDown.click()
+
+                try:
+                    breakfastOption = await page.waitForXPath('//div[text()="Lunch"]', timeout=5000)
+                    await breakfastOption.click()
+                except:
+                    await ctx.send("Sorry but it does not appear that there is a Lunch option today at Manzanita dining...")
+
+                doneButton = await page.waitForSelector('#modal-root > div > div > div > div > div.sc-cCsOjp.gvlGSX > button.sc-bczRLJ.sc-gsnTZi.gObyWR.SlTeX.Done', timeout=5000)
+
+                await doneButton.click()
+                await page.waitForSelector('.ChoosenMeal', timeout=60000)
+
+                await asyncio.sleep(2)
+                    
+            await scrape_food('//*[@id="13988"]', 'Daily Root')
+            await scrape_food('//*[@id="9139"]', 'Home Zone')
+            await scrape_food('//*[@id="9138"]', 'Grill')
+            await scrape_food('//*[@id="9140"]', 'Pizza')
+            await scrape_food('//*[@id="13991"]', 'Sazon Station')
+            
+            dailyRoot = food_dict['Daily Root']
+            dailyRoot = ', '.join(dailyRoot)
+            
+            homeZone = food_dict['Home Zone']
+            homeZone = ', '.join(homeZone)
+            
+            grill = food_dict['Grill']
+            grill = ', '.join(grill)
+            
+            pizza = food_dict['Pizza']
+            pizza = ', '.join(pizza)
+            
+            sazonStation = food_dict['Sazon Station']
+            sazonStation = ', '.join(sazonStation)
+            
+            embed = discord.Embed(title='Manzanita Dining Hall Lunch', description= 'Manzanita Dining Hall Lunch Menu')
+        
+            embed.set_thumbnail(url="https://i.imgur.com/DKR50qf.jpg")
+            
+            try:
+                embed.add_field(name="Daily Root:", value=dailyRoot, inline=False)
+            except:
+                pass
+            
+            try:
+                embed.add_field(name="Home Zone:", value=homeZone, inline=False)
+            except:
+                pass
+            
+            try:
+                embed.add_field(name='Sazon Station:', value=sazonStation, inline=False)
+            except:
+                pass
+            
+            try:
+                embed.add_field(name='Grill:', value=grill, inline=False)
+            except:
+                pass
+            
+            try:
+                embed.add_field(name='Pizza', value=pizza, inline=True)
+            except:
+                pass
+
+            embed.set_image(url="https://i.imgur.com/GSx3eR2.png")
+
+    if(requestedMeal == 'dinner'):
+        
+        await asyncio.sleep(2)
+
+        meal = await page.querySelector('.ChoosenMeal')
+        meal = await page.evaluate('(element) => element.textContent', meal)
+
+        if meal != 'Dinner':
+            try:
+                swapButton = await page.waitForSelector('.DateMealFilterButton', timeout=5000)
+                await swapButton.click()
+            except:
+                pass
+
+            dropDown = await page.waitForSelector('.css-1t70p0u-control', timeout=5000)
+            await dropDown.click()
+
+            try:
+                breakfastOption = await page.waitForXPath('//div[text()="Dinner"]', timeout=5000)
+                await breakfastOption.click()
+            except:
+                await ctx.send("Sorry but it does not appear that there is a Dinner option today at Manzanita dining...")
+
+            doneButton = await page.waitForSelector('#modal-root > div > div > div > div > div.sc-cCsOjp.gvlGSX > button.sc-bczRLJ.sc-gsnTZi.gObyWR.SlTeX.Done', timeout=5000)
+
+            await doneButton.click()
+            await page.waitForSelector('.ChoosenMeal', timeout=60000)
+
+            await asyncio.sleep(2)
+                
+        await scrape_food('//*[@id="13988"]', 'Daily Root')
+        await scrape_food('//*[@id="9139"]', 'Home Zone')
+        await scrape_food('//*[@id="9138"]', 'Grill')
+        await scrape_food('//*[@id="9140"]', 'Pizza')
+        await scrape_food('//*[@id="13991"]', 'Sazon Station')
+        
+        dailyRoot = food_dict['Daily Root']
+        dailyRoot = ', '.join(dailyRoot)
+        
+        homeZone = food_dict['Home Zone']
+        homeZone = ', '.join(homeZone)
+        
+        grill = food_dict['Grill']
+        grill = ', '.join(grill)
+        
+        pizza = food_dict['Pizza']
+        pizza = ', '.join(pizza)
+        
+        sazonStation = food_dict['Sazon Station']
+        sazonStation = ', '.join(sazonStation)
+        
+        embed = discord.Embed(title='Manzanita Dining Hall Dinner', description= 'Manzanita Dining Hall Dinner Menu')
+    
+        embed.set_thumbnail(url="https://i.imgur.com/DKR50qf.jpg")
+        
+        try:
+            embed.add_field(name="Daily Root:", value=dailyRoot, inline=False)
+        except:
+            pass
+        
+        try:
+            embed.add_field(name="Home Zone:", value=homeZone, inline=False)
+        except:
+            pass
+        
+        try:
+            embed.add_field(name='Sazon Station:', value=sazonStation, inline=False)
+        except:
+            pass
+        
+        try:
+            embed.add_field(name='Grill:', value=grill, inline=False)
+        except:
+            pass
+        
+        try:
+            embed.add_field(name='Pizza', value=pizza, inline=True)
+        except:
+            pass
+
+        embed.set_image(url="https://i.imgur.com/GSx3eR2.png")
+
+    await ctx.send(embed=embed)
+
+
 
 @bot.command()
 @commands.is_owner()
 async def shutdown(context):
     exit()     
-    
+
+
 bot.run(TOKEN)
     
